4d0079714bae9da25e27ba1bec49f282c620a181
Iris <mdza.iris@gmail.com>
2017-06-13 11:41:11 -0700

    wrap main app component with NavigationProvider

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-
-import {
-  AppRegistry,
-  Text, View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
 
 import { NavigationProvider, StackNavigation } from '@expo/ex-navigation';
 import { StatusBar } from 'react-native';
