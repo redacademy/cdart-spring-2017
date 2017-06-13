2f70f749aa086e480b11755a20206b3394570983
Johnwat92 <JohnWat92@gmail.com>
2017-06-13 10:35:37 -0700

    added reusable homescreen tab components

---
+++ js/components/HomeNavigationTab/HomeNavigationTab.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Image} from 'react-native';
+import { styles } from './styles';
+
+const HomeNavigationTab = ({title}) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+       style={{width: 75, height: 75}} />
+      <Text>{title}</Text>
+    </View>
+  );
+}
+
+export default HomeNavigationTab;
---
+++ js/components/HomeNavigationTab/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    margin: 4,
+    padding: 4,
+    marginTop: 32,
+    borderWidth: 2,
+    width: 150,
+    alignItems: 'center',
+    borderRadius: 7,
+    borderTopWidth: 10,
+    borderTopColor: 'blue',
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  }
+});
