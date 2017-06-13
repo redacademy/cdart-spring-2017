48cef7b6902937ab717a18d9f0c0f268d0cc1af5
Montana Sawyer <montanarsawyer@gmail.com>
2017-06-13 10:31:18 -0700

    add onboarding container

---
+++ js/scenes/Onboarding/Onboarding.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Onboarding extends Component {
+  state = {  }
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Onboarding;
--- js/scenes/Onboarding/OnboardingContainer.js
+++ js/scenes/Onboarding/OnboardingContainer.js
@@ -1,1 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
+import Onboarding from './Onboarding';
+
+class OnboardingContainer extends Component {
+  state = {  }
+  render() {
+    return (
+      <Onboarding />
+    );
+  }
+}
+
+export default OnboardingContainer;
