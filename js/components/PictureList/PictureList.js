8b9cda50f081b13e1d25830bd2338d058581976f
Johnwat92 <JohnWat92@gmail.com>
2017-06-13 15:27:20 -0700

    create data file

--- js/components/PictureList/PictureList.js
+++ js/components/PictureList/PictureList.js
@@ -9,6 +9,7 @@ class PictureList extends Component {
     super();
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
+      //CREATE A PROPERTY CALLED list and PASS AN ARRAY INTO IT
       dataSource: ds.cloneWithRows(this.props.list),
     }
   }
