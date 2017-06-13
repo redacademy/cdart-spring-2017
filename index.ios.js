4205b2fa24b8efa25bd2ce4cc62f3c21233f7d64
Johnwat92 <JohnWat92@gmail.com>
2017-06-13 11:27:40 -0700

    updated homeNavTabs

--- index.ios.js
+++ index.ios.js
@@ -3,21 +3,11 @@ import {
   AppRegistry,
   Text, View
 } from 'react-native';
-import HomeNavigationTab from './js/components/HomeNavigationTab';
 
 export default class cdart extends Component {
   render() {
-    var styles = {
-      container: {
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-      }
-    }
     return (
-      <View style={styles.container}>
-        <HomeNavigationTab title="pet profiles"/>
-        <HomeNavigationTab title="some stuff"/>
-      </View>
+      <Text>hello </Text>
     );
   }
 }
