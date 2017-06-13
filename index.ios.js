58f0c42e5868177dcc2d98fcc3ebd9c9ccd4265e
Iris <mdza.iris@gmail.com>
2017-06-13 11:41:11 -0700

    wrap main app component with NavigationProvider

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Text, View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+
+import { NavigationProvider, StackNavigation } from '@expo/ex-navigation';
+import { StatusBar } from 'react-native';
+
+import Router from './js/navigation/router';
 
 export default class cdart extends Component {
   render() {
     return (
-      <Text>hello </Text>
+      <NavigationProvider router={Router}>
+        <StatusBar barStyle="light-content" />
+        <StackNavigation
+          navigatorUID="root"
+          initialRoute={Router.getRoute('navigation')} />
+      </NavigationProvider>
     );
   }
 }
