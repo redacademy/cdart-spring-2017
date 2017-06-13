71c7caad7d3dd3e41b7b349fbc680a12059111f8
Johnwat92 <JohnWat92@gmail.com>
2017-06-13 13:16:23 -0700

    starting on the reusable picture list

--- index.ios.js
+++ index.ios.js
@@ -2,5 +2,14 @@ import {
   AppRegistry,
 } from 'react-native';
 import cdart from './js';
+import HomeNavigationTab from './js/components/HomeNavigationTab';
+
+export default class cdart extends Component {
+  render() {
+    return (
+        <Text>Hello</Text>
+    );
+  }
+}
 
 AppRegistry.registerComponent('cdart', () => cdart);
--- js/components/PictureList/PictureList.js
+++ js/components/PictureList/PictureList.js
@@ -20,7 +20,6 @@ const whichPicture = (title) => {
   }
 }
 
-
 class PictureList extends Component {
 
   constructor(props){
