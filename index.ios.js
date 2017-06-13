bc862769d33b9f27ac4338a667cdb05f7328e029
Johnwat92 <JohnWat92@gmail.com>
2017-06-13 14:17:20 -0700

    updated pictureList

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,7 @@ import {
   Text
 } from 'react-native';
 import HomeNavigationTab from './js/components/HomeNavigationTab';
+import PictureList from './js/components/PictureList';
 
 import { NavigationProvider, StackNavigation } from '@expo/ex-navigation';
 import { StatusBar } from 'react-native';
@@ -14,12 +15,17 @@ import Router from './js/navigation/router';
 export default class cdart extends Component {
   render() {
     return (
+<<<<<<< HEAD
       <NavigationProvider router={Router}>
         <StatusBar barStyle="light-content" />
         <StackNavigation
           navigatorUID="root"
           initialRoute={Router.getRoute('navigation')} />
       </NavigationProvider>
+=======
+        // <Text>Hello</Text>
+        <PictureList />
+>>>>>>> updated pictureList
     );
   }
 }
---
+++ js/components/PictureList/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+var { height, width } = Dimensions.get('window');
+
+export const styles = StyleSheet.create({
+  separator: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E',
+  },
+  textList: {
+    padding: 16,
+    fontSize: 16
+  },
+  singleItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+});
