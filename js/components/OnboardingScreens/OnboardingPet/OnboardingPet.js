c3368cb37a4651e32a99d8df7acd67c7b0c52206
Montana Sawyer <montanarsawyer@gmail.com>
2017-06-13 12:39:17 -0700

    add onboarding scenes

--- js/components/OnboardingScreens/OnboardingPet/OnboardingPet.js
+++ js/components/OnboardingScreens/OnboardingPet/OnboardingPet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+<<<<<<< HEAD
   Text,
   View
 } from 'react-native';
@@ -22,5 +23,27 @@ const OnboardingPet = () => {
       </LinearGradient>
     );
   }
+=======
+  View,
+  Text,
+  Image,
+  TouchableHighlight,
+} from 'react-native';
+
+const OnboardingPet = () => {
+  return (
+    <View>
+      <Text >Welcome to CDART Assist</Text>
+      <Text >Assisting you and your domestic animals in preparation for an emergency</Text>
+      <TouchableHighlight onPress={console.log('here')}>
+        <Text> Hello </Text>
+      </TouchableHighlight>
+      <TouchableHighlight onPress={console.log('here')}>
+        <Text> Hello </Text>
+      </TouchableHighlight>
+    </View>
+  );
+}
+>>>>>>> add onboarding scenes
 
 export default OnboardingPet;
--- js/scenes/Onboarding/OnboardingContainer.js
+++ js/scenes/Onboarding/OnboardingContainer.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import { connect } from 'react-redux';
 >>>>>>> add onboarding container
+=======
+>>>>>>> add onboarding scenes
 
 import Onboarding from './Onboarding';
 
