682b0b043b422d8dcf4e9f36d78a6e638c735993
andrew.nelsonrd@gmail.com <andrew.nelsonrd@gmail.com>
2017-06-14 13:33:52 -0700

    Fix index.ios.js after testing out new component with it.

--- index.ios.js
+++ index.ios.js
@@ -9,14 +9,17 @@ import HomeNavigationTab from './js/components/HomeNavigationTab';
 import { NavigationProvider, StackNavigation } from '@expo/ex-navigation';
 import { StatusBar } from 'react-native';
 
-import PetProfile from './js/scenes/PetProfile'
-
 import Router from './js/navigation/router';
 
 export default class cdart extends Component {
   render() {
     return (
-      <PetProfile />
+      <NavigationProvider router={Router}>
+        <StatusBar barStyle="light-content" />
+        <StackNavigation
+          navigatorUID="root"
+          initialRoute={Router.getRoute('navigation')} />
+      </NavigationProvider>
     );
   }
 }
