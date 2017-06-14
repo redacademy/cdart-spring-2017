595f0b869fa93c8ab010e1f75ebea0a06f3b3235
andrew.nelsonrd@gmail.com <andrew.nelsonrd@gmail.com>
2017-06-14 10:56:27 -0700

    Remove style causing warnings from petprofile

--- index.ios.js
+++ index.ios.js
@@ -9,17 +9,14 @@ import HomeNavigationTab from './js/components/HomeNavigationTab';
 import { NavigationProvider, StackNavigation } from '@expo/ex-navigation';
 import { StatusBar } from 'react-native';
 
+import PetProfile from './js/scenes/PetProfile'
+
 import Router from './js/navigation/router';
 
 export default class cdart extends Component {
   render() {
     return (
-      <NavigationProvider router={Router}>
-        <StatusBar barStyle="light-content" />
-        <StackNavigation
-          navigatorUID="root"
-          initialRoute={Router.getRoute('navigation')} />
-      </NavigationProvider>
+      <PetProfile />
     );
   }
 }
