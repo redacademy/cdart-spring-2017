20fbb97d3771f2e99438f9993355f8c8255e6f32
Johnwat92 <JohnWat92@gmail.com>
2017-06-14 11:52:14 -0700

    created new branch for pictureStory

--- js/components/PictureStory/PictureStory.js
+++ js/components/PictureStory/PictureStory.js
@@ -4,19 +4,40 @@ import { styles } from './styles';
 
 var { height, width } = Dimensions.get('window');
 
+
 class PictureStory extends Component {
+  renderContent(){
+    if(this.props.content.title1){
+      return (
+        <View>
+          <Text style={styles.header}>{this.props.content.title1}</Text>
+          <Text>{this.props.content.paragraph1}</Text>
+          {this.renderMoreContent()}
+        </View>
+
+      );
+    }
+  }
+  renderMoreContent(){
+    if(this.props.content.title2){
+      return (
+        <View>
+          <Text style={styles.header}>{this.props.content.title2}</Text>
+          <Text>{this.props.content.paragraph2}</Text>
+        </View>
+      );
+    }
+  }
   render() {
     return (
       <View>
         <Image source={{uri: 'https://s-media-cache-ak0.pinimg.com/736x/73/91/86/7391863170be07a072ffeb3e7605db2a.jpg'}}
         style={{width: width, height: 200}} />
-        <Text>About the app</Text>
-        <Text>Canadian Disaster Animal Response Team</Text>
-        <Text>CDART volunteers respond in times of disaster and emergencies when domestic animals are in need of rescue and sheltering. CDART is 100% volunteer based and 100%  of our funding comes from donations.</Text>
+        {this.renderContent()}
+        {this.renderMoreContent()}
       </View>
     );
   }
 }
 
 export default PictureStory;
-
