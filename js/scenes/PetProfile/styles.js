1847ebba4c514ed5e5baf170780a12bf35fe03f4
andrew.nelsonrd@gmail.com <andrew.nelsonrd@gmail.com>
2017-06-14 10:56:27 -0700

    Remove style causing warnings from petprofile

--- js/scenes/PetProfile/styles.js
+++ js/scenes/PetProfile/styles.js
@@ -1,5 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-
+  createProfile: {
+    flex: 1,
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
 });
