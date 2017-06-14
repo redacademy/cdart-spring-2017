5cbfc5740c98c447d80b7648aa29778f6447e8dc
Johnwat92 <JohnWat92@gmail.com>
2017-06-14 16:42:24 -0700

    removed tests scenes from updates

--- js/scenes/Updates/Updates.js
+++ js/scenes/Updates/Updates.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
 import { Text } from 'react-native';
-import PictureStory from '../../components/PictureStory';
-
-import {cdartMission, takeACourse} from '../../assets/data';
 
 const Updates = () => {
   return (
-    // <Text>************ UPDATES ***********</Text>
-    <PictureStory content={cdartMission} />
+    <Text>************ UPDATES ***********</Text>
   );
 }
 
