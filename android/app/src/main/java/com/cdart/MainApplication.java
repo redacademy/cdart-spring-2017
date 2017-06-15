99f8a2566b519335ce7335daca73c63e4ddecbf1
Evan Gray <evanlawgray@gmail.com>
2017-06-15 10:05:39 -0700

    Remove splash screen package.

--- android/app/src/main/java/com/cdart/MainApplication.java
+++ android/app/src/main/java/com/cdart/MainApplication.java
@@ -3,7 +3,6 @@ package com.cdart;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.cboy.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SplashScreenReactPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage()
       );
