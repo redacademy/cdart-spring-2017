f9e5bec0733b4035aea00eaf1f342a4f0932a48b
andrew.nelsonrd@gmail.com <andrew.nelsonrd@gmail.com>
2017-06-15 09:23:39 -0700

    Remove local branch feature-updates.

--- js/redux/reducers.js
+++ js/redux/reducers.js
@@ -3,6 +3,6 @@ import { combineReducers } from 'redux';
 import { NavigationReducer } from '@expo/ex-navigation';
 
 export default combineReducers({
-  navigation: NavigationReducer
+  navigation: NavigationReducer,
 });
 
