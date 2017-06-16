42819d98b4773a0e185d7947c676f5ff85276518
Warren Chan <warrenchan@warren-2.local>
2017-06-16 12:05:58 -0700

    intake form folder rename for better understanding

--- js/scenes/Updates/Updates.js
+++ js/scenes/Updates/Updates.js
@@ -1,12 +1,11 @@
-import React from 'react';
+ import React from 'react';
 
 import { Text } from 'react-native';
-import IntakeForm from './../../components/Forms/IntakeForm';
 
 const Updates = () => {
   return (
     <Text>Updates...</Text>
   );
-};
+}
 
 export default Updates;
