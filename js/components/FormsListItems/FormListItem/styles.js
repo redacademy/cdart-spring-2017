39950e5f62052b50424350ab1aa3b84274cb0469
Warren Chan <warrenchan@warren-2.local>
2017-06-16 09:56:59 -0700

    Remove app placeholder icons.

--- js/components/FormsListItems/FormListItem/styles.js
+++ js/components/FormsListItems/FormListItem/styles.js
@@ -3,7 +3,7 @@ import { colors, typography } from './../../../config/styles';
 
 export const styles = StyleSheet.create({
     listItem: {
-        backgroundColor: colors.textBackgroundGrey,
+        backgroundColor: colors.lightGrey,
         marginBottom: 6,
         paddingTop: 10,
         paddingBottom: 10,
