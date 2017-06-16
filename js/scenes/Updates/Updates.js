6e41faa3be6ecf6dcda532c26615534ca1e16fdf
Evan Gray <evanlawgray@gmail.com>
2017-06-16 15:16:41 -0700

    Remove merge conflict header from Updates.js

--- js/scenes/Updates/Updates.js
+++ js/scenes/Updates/Updates.js
@@ -1,19 +1,9 @@
  import React from 'react';
-<<<<<<< HEAD
-
-import ReceptionContainer from '../ReceptionCentre';
-import AnimalRescue from '../AnimalRescue';
-
-const Updates = () => {
-  return (
-    <AnimalRescue />
-=======
  import { Text } from 'react-native';
 
 const Updates = () => {
   return (
     <Text>updates....</Text>
->>>>>>> Added fonts to assets
   );
 }
 
