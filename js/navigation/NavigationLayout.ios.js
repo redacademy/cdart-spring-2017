fd265138ef17f8ed6bbd122b62e3db5ecd716860
Montana Sawyer <montanarsawyer@gmail.com>
2017-06-19 11:21:54 -0700

    Add icons and implement color change on nav bar

--- js/navigation/NavigationLayout.ios.js
+++ js/navigation/NavigationLayout.ios.js
@@ -7,7 +7,7 @@ import {
 import LinearGradient from 'react-native-linear-gradient';
 
 import Router from './router';
-import { Text, StyleSheet } from 'react-native';
+import { Text, Image, StyleSheet } from 'react-native';
 import { colors, typography, gradients } from '../config/styles';
 
 const defaultRouteConfig = {
@@ -41,9 +41,30 @@ class NavigationLayout extends Component {
     }
   }
 
+  renderIcon(iconName, isSelected) {
+    const whichIcon = (icon, isSelected) => {
+      switch(icon){
+        case "home":
+          return isSelected ? require('../assets/icons/red_home_icon.png'): require('../assets/icons/outline_home_icon.png') ;
+        case "petProfile":
+          return isSelected ? require('../assets/icons/paw_icon.png'): require('../assets/icons/pet_profile_icon.png') ;
+        case "updates":
+          return isSelected ? require('../assets/icons/red_updates_icon.png'): require('../assets/icons/updates_icon.png') ;
+        case "getInvolved":
+          return isSelected ? require('../assets/icons/red_involved_icon.png'): require('../assets/icons/outline_involved_icon.png') ;
+        default:
+          return ;
+      }
+    }
+    return <Image
+            source={whichIcon(iconName, isSelected)}
+            style={{width: 30, height: 30, resizeMode: 'contain'}}
+          />
+  }
+
   renderTitle(isSelected, title) {
     const titleStyle = {
-      color: isSelected ? colors.red : colors.lightGrey,
+      color: isSelected ? colors.red : colors.grey,
       fontSize: 12,
       fontFamily: 'Arial'
     };
@@ -61,7 +82,7 @@ class NavigationLayout extends Component {
         <TabItem
           id="home"
           title="Home"
-          // renderIcon={isSelected => this.renderIcon('ios-calendar', isSelected)}
+          renderIcon={isSelected => this.renderIcon("home", isSelected)}
           renderTitle={this.renderTitle}
         >
           <StackNavigation
@@ -75,6 +96,7 @@ class NavigationLayout extends Component {
         <TabItem
           id="petProfile"
           title="Pet Profile"
+          renderIcon={isSelected => this.renderIcon("petProfile", isSelected)}
           renderTitle={this.renderTitle}
         >
           <StackNavigation
@@ -88,6 +110,7 @@ class NavigationLayout extends Component {
         <TabItem
           id="updates"
           title="Updates"
+          renderIcon={isSelected => this.renderIcon("updates", isSelected)}
           renderTitle={this.renderTitle}
         >
           <StackNavigation
@@ -101,6 +124,7 @@ class NavigationLayout extends Component {
         <TabItem
           id="getInvolved"
           title="Get Involved"
+          renderIcon={isSelected => this.renderIcon("getInvolved", isSelected)}
           renderTitle={this.renderTitle}
         >
           <StackNavigation
