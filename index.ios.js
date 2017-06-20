502a72e75a91b17d816a903de2a386313ada7bd9
Evan Gray <evanlawgray@gmail.com>
2017-06-20 12:13:07 -0700

    Fix merge conflicts with bugfix-displaypdf

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,28 @@
 import {
   AppRegistry,
 } from 'react-native';
+<<<<<<< .merge_file_Ov32of
 import cdart from './js';
+=======
+import HomeNavigationTab from './js/components/HomeNavigationTab';
+
+import { NavigationProvider, StackNavigation } from '@expo/ex-navigation';
+import { StatusBar } from 'react-native';
+
+import Router from './js/navigation/router';
+
+export default class cdart extends Component {
+  render() {
+    return (
+      <NavigationProvider router={Router}>
+        <StatusBar barStyle="light-content" />
+        <StackNavigation
+          navigatorUID="root"
+          initialRoute={Router.getRoute('navigation')} />
+      </NavigationProvider>
+    );
+  }
+}
+>>>>>>> .merge_file_r1P9fE
 
 AppRegistry.registerComponent('cdart', () => cdart);
