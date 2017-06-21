3e1bf5441355a940a18ec3e04b912a7c2709608d
Evan Gray <evanlawgray@gmail.com>
2017-06-21 15:16:44 -0700

    Add titles to procedure list and subpages. Rename ReceptionCenter component to ProcedureList.

--- js/scenes/AnimalRescue/AnimalRescue.js
+++ js/scenes/AnimalRescue/AnimalRescue.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
 import { WebView } from 'react-native';
+=======
+import { Image } from 'react-native';
+>>>>>>> Add titles to procedure list and subpages. Rename ReceptionCenter component to ProcedureList.
 
 import { styles } from './styles'
 
