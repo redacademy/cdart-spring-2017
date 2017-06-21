847dd97b74dc0c574714152fc4508a65a4a1ee1b
Iris <mdza.iris@gmail.com>
2017-06-21 14:15:09 -0700

    Implement openSelectDialog method in ImagePicker component.

--- js/scenes/ImagePicker/ImagePicker.js
+++ js/scenes/ImagePicker/ImagePicker.js
@@ -3,16 +3,39 @@ import {
   ScrollView,
   View,
   Text,
+  Image,
+  ImagePickerIOS,
 } from 'react-native';
 
 import { styles } from './styles';
 
 class ImagePicker extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+    };
+  }
+
+  componentDidMount() {
+    this.pickImage();
+  }
+
+  pickImage() {
+    // openSelectDialog(config, successCallback, errorCallback);
+    ImagePickerIOS.openSelectDialog({}, imageUri => {
+      this.setState({ image: imageUri });
+    }, error => console.log(error));
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Text>ADD IMAGE</Text>
+        {this.state.image?
+          <Image style={{ flex: 1, alignSelf: 'center', width: 210, height: 140, marginTop: 5, marginBottom: 15, borderRadius: 15 }} source={{ uri: this.state.image }} /> :
+          null
+        }
       </View>
     );
   }
