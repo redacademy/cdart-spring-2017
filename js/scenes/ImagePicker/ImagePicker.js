132dd5d8ca2e38986389fcebac813bda68bcc50c
Iris <mdza.iris@gmail.com>
2017-06-21 14:13:07 -0700

    Add ImagePicker scene.

---
+++ js/scenes/ImagePicker/ImagePicker.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+  ScrollView,
+  View,
+  Text,
+} from 'react-native';
+
+import { styles } from './styles';
+
+class ImagePicker extends Component {
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>ADD IMAGE</Text>
+      </View>
+    );
+  }
+}
+
+export default ImagePicker;
---
+++ js/scenes/ImagePicker/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+import { colors, typography } from '../../config/styles';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  imageGrid: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 10,
+  },
+});
