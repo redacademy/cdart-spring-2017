a7328f9ab76b31a2041ddbc7923813e422b22477
Montana Sawyer <montanarsawyer@gmail.com>
2017-06-21 16:14:03 -0700

    Configure realm schemas

--- js/config/models.js
+++ js/config/models.js
@@ -3,7 +3,34 @@ import Realm from 'realm';
 const Temperament = {
   name: 'Temperament',
   properties: {
+    temperament: 'string',
+  }
+}
 
+const Owner = {
+  name: 'Owner',
+  properties: {
+    name: 'string',
+    phone: 'int',
+    email: 'string',
+    street: 'string',
+    city: 'string',
+    province: 'string',
+    postal: 'string',
+  }
+}
+
+const Vet = {
+  name: 'Vet',
+  properties: {
+    name: 'string',
+    hospital: 'string',
+    phone: 'int',
+    email: 'string',
+    street: 'string',
+    city: 'string',
+    province: 'string',
+    postal: 'string',
   }
 }
 
@@ -12,6 +39,7 @@ const PetProfile = {
   primaryKey: 'id',
   properties: {
     id: 'int',
+    image: 'string',
     name: 'string',
     age: 'int',
     species: 'string',
@@ -19,8 +47,15 @@ const PetProfile = {
     color: 'string',
     sex: 'string',
     neutered: 'bool',
-    temperament: 'string',
+    temperaments: {type: 'list', objectType: 'Temperament'},
+    temperInfo: 'string',
     features: 'string',
-
+    care: 'string',
+    medicalAlert: 'string',
+    microchip: 'string',
+    owners: {type: 'list', objectType: 'Owner'},
+    vet: 'Vet',
   }
 }
+
+let realm = new Realm({schema: [Temperament, PetProfile, Owner, Vet]});
