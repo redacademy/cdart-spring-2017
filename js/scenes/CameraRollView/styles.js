9d29db6153579a62e6b56ca4f3bb7a2085a8bf5e
Iris <mdza.iris@gmail.com>
2017-06-22 16:45:34 -0700

    Add Dimensions for styling of CameraRollView.

--- js/scenes/CameraRollView/styles.js
+++ js/scenes/CameraRollView/styles.js
@@ -2,19 +2,17 @@ import { StyleSheet } from 'react-native';
 import { colors, typography } from '../../config/styles';
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignSelf: 'center',
-    marginTop: 5,
-    marginBottom: 15,
-    borderWidth: 1,
-    borderColor: colors.skyBlue,
-    borderRadius: 15,
-    overflow: 'hidden'
+  imageGrid: {
+    // flex: 1,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    // alignSelf: 'center',
+    // marginTop: 5,
+    // marginBottom: 15,
+    // borderRadius: 15,
+    // overflow: 'hidden'
   },
-  image: {
-    flex: 1,
-    width: 210,
-    height: 140
-  }
+  // image: {
+
+  // }
 });
