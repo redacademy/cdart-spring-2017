37e57c2a287ffa2e9b4bb3f2a7416e957c244a9b
Iris <mdza.iris@gmail.com>
2017-06-22 16:00:12 -0700

    Add ImageUploader component.

---
+++ js/components/PetProfileForm/ImageUploader/ImageUploader.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {
+  Image,
+  TouchableHighlight
+} from 'react-native';
+
+import { colors } from '../../../config/styles';
+import { styles } from './styles';
+
+class ImageUploader extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+    };
+  }
+
+  render() {
+    console.log(this.props, this.state);
+    return (
+      <TouchableHighlight
+        underlayColor={colors.grey}
+        style={styles.container}
+      >
+        <Image
+          style={styles.image}
+          source={
+            this.state.image ? { uri: this.state.image }
+            : require('../../../assets/icons/paw_icon.png')}
+        />
+      </TouchableHighlight>
+    );
+  }
+}
+
+export default ImageUploader;
