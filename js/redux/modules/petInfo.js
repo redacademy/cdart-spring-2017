b092f38968966b9e029a34367a39cfe7af6848aa
Iris <mdza.iris@gmail.com>
2017-06-23 16:07:35 -0700

    Add setPetProfileImageUri reducer for pet profile form.

--- js/redux/modules/petInfo.js
+++ js/redux/modules/petInfo.js
@@ -1,13 +1,18 @@
 export const ADD_TEMPERMENT_INFO = 'ADD_TEMPERMENT_INFO';
 export const ADD_GENDER_INFO = 'ADD_GENDER_INFO';
 export const ADD_SPAYED = 'ADD_SPAYED';
+export const SET_PET_PROFILE_IMAGE_URI = 'SET_PET_PROFILE_IMAGE_URI';
 
 export const addTemperment = (info) => ({ type: ADD_TEMPERMENT_INFO, payload: info });
 export const addGender = (info) => ({ type: ADD_GENDER_INFO, payload: info });
 export const addSpayed = (info) => ({ type: ADD_SPAYED, payload: info });
+export const setPetProfileImageUri = (uri) => ({ type: SET_PET_PROFILE_IMAGE_URI, payload: uri });
 
 export function reducer(state = {
-    temperments: []
+    temperments: [],
+    gender: '',
+    spayed: undefined,
+    selectedImage: ''
 }, action) {
     switch (action.type) {
     case ADD_TEMPERMENT_INFO:{
@@ -23,6 +28,11 @@ export function reducer(state = {
     case ADD_SPAYED: {
         return Object.assign({}, state, {
             spayed: action.payload
+        })
+    }
+    case SET_PET_PROFILE_IMAGE_URI: {
+        return Object.assign({}, state, {
+            selectedImage: action.payload
         })
     }
     default:
