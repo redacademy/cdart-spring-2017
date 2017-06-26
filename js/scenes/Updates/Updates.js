8c6248cfdefa0db93b71ea5a086f005a6f4db386
Johnwat92 <JohnWat92@gmail.com>
2017-06-26 15:46:05 -0700

    added refresh fetch on scroll up

--- js/scenes/Updates/Updates.js
+++ js/scenes/Updates/Updates.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text, AppState } from 'react-native';
+import { View, Text, AppState, Image, Dimensions, ScrollView, RefreshControl } from 'react-native';
 import PropTypes from 'prop-types';
-
+import { _fetchTwitter } from '../../redux/modules/updates';
 import PushController from './../../pushController';
+import { connect } from 'react-redux'
+
 import PushNotification from 'react-native-push-notification'
 
+var {height, width} = Dimensions.get('window');
 class Updates extends Component {
   constructor(props) {
     super(props);
     this.handleAppListener = this.handleAppListener.bind(this);
+    this.state={
+      refreshing:false,
+    };
   }
 
   componentDidMount() {
@@ -26,28 +32,66 @@ class Updates extends Component {
       });
     }
   }
+   _onRefresh() {
+    this.setState({refreshing: true});
+    this.props.fetchTwitter();
+    this.setState({refreshing: false});
+  }
 
   render() {
     console.log('passed Data', this.props.tweets)
     return (
-    <View style={{padding:16}}>
+    <ScrollView
+      style={{padding:16}}
+      refreshControl={
+        <RefreshControl
+          refreshing={this.state.refreshing}
+          onRefresh={this._onRefresh.bind(this)}
+        />
+      }
+    >
       <View>
-        {(this.props.tweets) ? this.props.tweets.map((tweet, i) => { return (
-          <View key={i} style={{marginBottom: 16}}>
-            <View style={{flexDirection:'row', justifyContent: 'space-between'}}>
-              <Text style={{fontWeight:'bold'}}>CDART</Text>
-              <Text>{tweet.created_at.substring(4, 10)}</Text>
+        {(this.props.tweets) ? this.props.tweets.map((tweet, i) => {
+          return (
+            <View
+              key={i}
+              style={{
+                flexDirection:'row',
+                marginBottom: 8,
+                paddingBottom: 8,
+                borderBottomWidth: .5,
+                borderColor:'grey',
+                justifyContent:'space-between'
+            }}>
+              <View
+                style={{
+                  width: width - 32,
+                  flexWrap: 'wrap'
+                }}
+              >
+                <View style={{flexDirection:'row', justifyContent: 'space-between', marginBottom: 4}}>
+                  <Text style={{fontWeight:'bold'}}>{tweet.user.name}
+                    <Text style={{color:'grey'}}> @{tweet.user.screen_name}</Text>
+                  </Text>
+                  <Text style={{color:'red'}}>{tweet.created_at.substring(4, 10)}</Text>
+                </View>
+                <View style={{width: width - 32}}>
+                  {tweet.entities.media ?
+                    <Image source={{uri: tweet.entities.media[0].media_url_https}} style={{width: '100%', height:250, marginBottom: 8, marginTop:4}} />
+                    :
+                    <View></View>
+                  }
+                  <Text>{tweet.text}</Text>
+                </View>
+              </View>
             </View>
-            <View>
-            <Text style={{width: 200}}>{tweet.text}</Text>
-            </View>
-          </View>
-        )})
+          )
+        })
         :
         <Text style={{textAlign: 'center'}}>Loading Tweets... </Text>}
       </View>
       <PushController />
-    </View>
+    </ScrollView>
     );
   }
 }
@@ -55,5 +99,17 @@ class Updates extends Component {
 Updates.propTypes = {
   tweets: PropTypes.array
 };
+function mapDispatchToProps(dispatch){
+  return {
+    fetchTwitter(){
+      dispatch(_fetchTwitter())
+    }
+  }
+}
+function mapStateToProps(state){
+  return {
+    updates: state.updates
+  }
+}
 
-export default Updates;
+export default connect(mapStateToProps, mapDispatchToProps)(Updates);
