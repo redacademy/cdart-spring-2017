a9bfca3573f5b38d1208fee61275e83f92afce5e
Warren Chan <warrenchan@warren-2.local>
2017-06-26 11:37:30 -0700

    Notifications triggered when app in background from updates

--- js/scenes/Updates/Updates.js
+++ js/scenes/Updates/Updates.js
@@ -8,11 +8,7 @@ import PushNotification from 'react-native-push-notification'
 class Updates extends Component {
   constructor(props) {
     super(props);
-
     this.handleAppListener = this.handleAppListener.bind(this);
-    this.state = {
-
-    };
   }
 
   componentDidMount() {
@@ -25,9 +21,8 @@ class Updates extends Component {
 
   handleAppListener(AppState) {
     if(AppState === 'background') {
-      PushNotification.localNotificationSchedule({
-        message: "My Notification Message", // (required)
-        date: new Date(Date.now() + (5 * 1000)) // in 60 secs
+      PushNotification.localNotification({
+        message: "Tweet Update", // (required)
       });
     }
   }
@@ -35,21 +30,22 @@ class Updates extends Component {
   render() {
     console.log('passed Data', this.props.tweets)
     return (
-      <View style={{padding:16}}>
-      {(this.props.tweets) ? this.props.tweets.map((tweet, i) => { return (
-        <View key={i} style={{marginBottom: 16}}>
-          <View style={{flexDirection:'row', justifyContent: 'space-between'}}>
-            <Text style={{fontWeight:'bold'}}>CDART</Text>
-            <Text>{tweet.created_at.substring(4, 10)}</Text>
+    <View style={{padding:16}}>
+      <View>
+        {(this.props.tweets) ? this.props.tweets.map((tweet, i) => { return (
+          <View key={i} style={{marginBottom: 16}}>
+            <View style={{flexDirection:'row', justifyContent: 'space-between'}}>
+              <Text style={{fontWeight:'bold'}}>CDART</Text>
+              <Text>{tweet.created_at.substring(4, 10)}</Text>
+            </View>
+            <View>
+            <Text style={{width: 200}}>{tweet.text}</Text>
+            </View>
           </View>
-          <View>
-          <Text style={{width: 200}}>{tweet.text}</Text>
-          </View>
-        </View>
-        )}
-      )
-      :
-      <Text style={{textAlign: 'center'}}>Loading Tweets... </Text>}
+        )})
+        :
+        <Text style={{textAlign: 'center'}}>Loading Tweets... </Text>}
+      </View>
       <PushController />
     </View>
     );
@@ -57,7 +53,7 @@ class Updates extends Component {
 }
 
 Updates.propTypes = {
-  tweets: PropTypes.string
+  tweets: PropTypes.array
 };
 
 export default Updates;
