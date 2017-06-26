043b592993b307caf52b5924066250a9c2984a91
Evan Gray <evanlawgray@gmail.com>
2017-06-26 09:58:49 -0700

    Map over temperaments and access actual temperament string where info is stored.

---
+++ js/redux/modules/fetchPets.js
@@ -0,0 +1,11 @@
+const GET_PETS_LOADING = 'GET_PETS_LOADING';
+const GET_PETS_ERROR = 'GET_PETS_ERROR';
+const GET_PETS = 'GET_PETS';
+
+const GET_PETS_LOADING = () => ({ type: GET_PETS_LOADING });
+const GET_PETS_ERROR = () => ({ type: GET_PETS_ERROR });
+const GET_PETS = () => ({ type: GET_PETS });
+
+export const fetchPets = () => {
+
+}
