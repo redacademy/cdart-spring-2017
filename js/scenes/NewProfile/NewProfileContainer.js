1fc967d207a405a1e52253555e6b01cff88fe9a0
Montana Sawyer <montanarsawyer@gmail.com>
2017-06-29 10:58:20 -0700

    Update reducers and corresponding files.

--- js/scenes/NewProfile/NewProfileContainer.js
+++ js/scenes/NewProfile/NewProfileContainer.js
@@ -26,9 +26,10 @@ class NewProfileContainer extends Component {
 
 function mapStateToProps(state) {
   return {
-    formData: state.petProfile,
+    formData: state.form,
+    formToggle: state.formToggle,
     formValues: getFormValues('NewProfileForm')(state),
-    selectedImage: state.petProfile.toggleInputs.selectedImage
+    selectedImage: state.formToggle.selectedImage
   }
 }
 
